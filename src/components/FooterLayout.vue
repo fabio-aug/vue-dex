4e214530f1ce75f75a52ef462e5e4f003ad43ec7
Fábio Augusto <fabio.augusto1911@gmail.com>
2023-03-09 20:00:37 +0000

    Configure bootstrap in vue project.

---
+++ src/components/FooterLayout.vue
@@ -0,0 +1,61 @@
+<script setup></script>
+
+<template>
+  <footer class="py-3 border-top">
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a
+          href="https://github.com/fabio-aug"
+          class="nav-link px-2 text-muted"
+          target="_blank"
+        >
+          Autor
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          href="#"
+          class="nav-link px-2 text-muted"
+          target="_blank"
+        >
+          API
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          href="https://vuejs.org/"
+          class="nav-link px-2 text-muted"
+          target="_blank"
+        >
+          Framework
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          href="https://getbootstrap.com/"
+          class="nav-link px-2 text-muted"
+          target="_blank"
+        >
+          Ui
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          href="https://github.com/fabio-aug/vue-dex"
+          class="nav-link px-2 text-muted"
+          target="_blank"
+        >
+          Sobre
+        </a>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  bottom: 0;
+  position: relative;
+  width: 100%;
+}
+</style>
---
+++ src/components/HeaderLayout.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+  <header class="d-flex justify-content-center py-3 border-bottom">
+    <ul class="nav nav-pills d-flex justify-content-center">
+      <li class="nav-item">
+        <a
+          href="#"
+          class="nav-link"
+        >
+          Nacional Dex
+        </a>
+      </li>
+      <li class="nav-item">
+        <a
+          href="#"
+          class="nav-link active"
+          aria-current="page"
+        >Home</a>
+      </li>
+      <li class="nav-item">
+        <a
+          href="#"
+          class="nav-link"
+        >
+          Regional Dex
+        </a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<style scoped></style>
